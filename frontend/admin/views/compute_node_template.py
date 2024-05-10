a52cb13ad156442589343b66a13b9acd2176e684
Nikola Sočec <nikola.socec@gmail.com>
2024-05-10 20:04:28 +0200

    Fixed incorrect image name.

--- frontend/admin/views/compute_node_template.py
+++ frontend/admin/views/compute_node_template.py
@@ -5,7 +5,7 @@ from frontend.utils.view import View
 
 class ComputeNodeTemplate(View):
     id: str
-    image: str = "nikolasocec/cosmollm-compute"
+    image: str = "nikolasocec/cosmollm-compute-node"
     cpuLimit: str = "2000m"
     memoryLimit: str = "4Gi"
     cpuRequest: Optional[str] = None
