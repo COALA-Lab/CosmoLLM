d0e6c1d2f85fb705fc31c4d80878fb0726d73518
assert-0 <49787293+assert-0@users.noreply.github.com>
2024-07-04 18:28:07 +0200

    Changed default GPT model to 4o.

--- llm_integrations/settings.py
+++ llm_integrations/settings.py
@@ -9,8 +9,8 @@ env.read_env(path=root_dir + "/.env")
 
 # OpenAI
 OPENAI_API_KEY = env.str("OPENAI_API_KEY", "")
-OPENAI_CHAT_GPT_MODEL = env.str("OPENAI_CHAT_GPT_MODEL", "gpt-3.5-turbo")
+OPENAI_CHAT_GPT_MODEL = env.str("OPENAI_CHAT_GPT_MODEL", "gpt-4o")
 
 
 # LangChain
-LANGCHAIN_CHAT_MODEL = env.str("LANGCHAIN_CHAT_MODEL", "gpt-3.5-turbo")
+LANGCHAIN_CHAT_MODEL = env.str("LANGCHAIN_CHAT_MODEL", "gpt-4o")
